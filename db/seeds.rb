5e356c06bb5c77fb5412dc81784fcc3f1d8f9f83
Bettenoch <bettenoch254@gmail.com>
2023-12-05 12:14:06 +0300

    create greetings seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,13 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# Create an array of greetings
+greetings = [
+  "Hello!",
+  "Hi there!",
+  "Welcome!",
+  "Greetings!",
+  "Good day!"
+]
+
+# Loop through the greetings array and create records in the greetings table
+greetings.each do |message|
+  Greeting.create!(message: message)
+end
---
+++ test/controllers/api/v1/greetings_controller_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class Api::V1::GreetingsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get api_v1_greetings_index_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get api_v1_greetings_create_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get api_v1_greetings_show_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get api_v1_greetings_destroy_url
+    assert_response :success
+  end
+end
