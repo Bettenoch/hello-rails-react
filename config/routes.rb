bca9febfd2a54cbac2ed8e9925678d9c76447a05
Bettenoch <bettenoch254@gmail.com>
2023-12-05 12:13:06 +0300

    Add routes for the controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :greetings, only: [:index, :create, :show, :destroy]
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
+  root 'root#index'
   # Defines the root path route ("/")
   # root "posts#index"
 end
